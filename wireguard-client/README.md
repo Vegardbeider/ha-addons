fd478c93697591c6dd01ac527cf961b08d925f33
Vegardbeider <vegard@sgjerde.net>
2024-08-27 14:21:55 +0200

    First configuration of addon

---
+++ wireguard-client/README.md
@@ -0,0 +1,13 @@
+# Wireguard Client
+![Supports aarch64 Architecture][aarch64-shield] ![Supports amd64 Architecture][amd64-shield]
+
+## Prerequisites
+
+## Installation
+
+## Configuration
+
+## Usage
+
+[aarch64-shield]: https://img.shields.io/badge/aarch64-yes-green.svg
+[amd64-shield]: https://img.shields.io/badge/amd64-yes-green.svg
---
+++ wireguard-client/rootfs/run.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/with-contenv bashio
+
+FILE1="/config/wg/privatekey"
+FILE2="/config/wg/publickey"
+if [ -f "$FILE1" ] && [ -f "$FILE2"]; then
+    echo "Privatekey and Publickey already exist"
+else
+    umask 077
+    /usr/bin/wg genkey | tee /config/privatekey | /usr/bin/wg pubkey > /config/publickey
+    echo "Privatekey and Publickey generated"
+fi
+
+FILE3="/config/wg/wg0.conf"
+if [ -f "$FILE3" ]; then
+    echo "Config file already exist"
+else
+    echo "[Interface]" > /config/wg/wg0.conf
+    echo "PrivateKey = $(cat /config/privatekey)" >> /config/wg/wg0.conf
+    echo "Address =" >> /config/wg/wg0.conf
+
+    echo "[Peer]" >> /config/wg/wg0.conf
+    echo "PublicKey" >> /config/wg/wg0.conf
+    echo "AllowedIPs =" >> /config/wg/wg0.conf
+    echo "Endpoint =" >> /config/wg/wg0.conf
+
+    echo "Created incomplete samlple config file"
+fi
+
+cp -r /config/wg /etc/wireguard/
+
+/usr/bin/supervisord -c /etc/supervisord.conf
